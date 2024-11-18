e0cd19c9718863070e0ad477081cd1096cff5b8f
AliGhanizade <thealighanizade@gmail.com>
2024-11-18 23:30:58 +0330

    Use componnet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const age = 1
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+{age > 20 ?<User name="Ali" age={20}/> : <Inputeforadd name="Reza"/>}
+</>
+  
   );
 }
-
+ const User = (propse)=>{
+  return (<div>
+    <h1>{propse.name}</h1>
+    <h2>{propse.age}</h2>
+  </div>
+  )
+}
+const Inputeforadd = (propse)=>{
+  return (
+    <> <button>{propse.name}</button> </>
+  )
+}
 export default App;
