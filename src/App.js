350cb04c6fe28163be09d32f36a190bbc0020fc0
AliGhanizade <thealighanizade@gmail.com>
2024-11-18 23:41:58 +0330

    usestate hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 //  import {} from "./componnet"
+
 function App() {
-  return <></>;
+  const [ count , setCount ] = useState(0)
+  return (<>
+  <button onClick={() => {setCount(count + 1 )}}>increse</button>
+  <button onClick={() => {setCount(count - 1 )}}>decrese</button>
+  <button onClick={() => {setCount(0)}}>set to 0</button>
+  <h1>{count}</h1>
+  </>);
 }
 
 export default App;
