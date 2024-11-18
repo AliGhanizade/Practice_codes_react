e09cb4dfcc768c858a6ea465e4e2fd5959cd79a6
AliGhanizade <thealighanizade@gmail.com>
2024-11-18 23:31:43 +0330

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 
-
 function App() {
-  const age = 1
+  const classes = [
+    { name: "html", finished: true },
+    { name: "Java script", finished: true },
+    { name: "css", finished: false },
+    { name: "C#", finished: true },
+    { name: "Php", finished: false },
+  ];
   return (
-<>
-{age > 20 ?<User name="Ali" age={20}/> : <Inputeforadd name="Reza"/>}
-</>
-  
-  );
+  <>
+  {classes.map ((c , index)=>{
+
+return  <Showclass key={index} name={c.name} finished={c.finished} />
+  }
+)}
+  </>);
 }
- const User = (propse)=>{
-  return (<div>
-    <h1>{propse.name}</h1>
-    <h2>{propse.age}</h2>
-  </div>
-  )
-}
-const Inputeforadd = (propse)=>{
-  return (
-    <> <button>{propse.name}</button> </>
-  )
-}
+// const User = (propse) => {
+//   return (
+//     <div>
+//       <h1>{propse.name}</h1>
+//       <h2>{propse.age}</h2>
+//     </div>
+//   );
+// };
+// const Inputeforadd = (propse) => {
+//   return (
+//     <>
+//       {" "}
+//       <button>{propse.name}</button>{" "}
+//     </>
+//   );
+// };
+
+const Showclass = (props) => {
+  if (props.finished === true) {
+    return <h2>{props.name}</h2>;
+  }
+};
 export default App;
