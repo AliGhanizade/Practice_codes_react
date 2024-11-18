b4f25aa6e5985ff09abc858ee8c9b8535c2c1d33
AliGhanizade <thealighanizade@gmail.com>
2024-11-18 23:36:57 +0330

    Cut componnet and create componnet file for use this

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
-
+//  import {} from "./componnet"
 function App() {
-  const classes = [
-    { name: "html", finished: true },
-    { name: "Java script", finished: true },
-    { name: "css", finished: false },
-    { name: "C#", finished: true },
-    { name: "Php", finished: false },
-  ];
-  return (
-  <>
-  {classes.map ((c , index)=>{
-
-return  <Showclass key={index} name={c.name} finished={c.finished} />
-  }
-)}
-  </>);
+  return <></>;
 }
-// const User = (propse) => {
-//   return (
-//     <div>
-//       <h1>{propse.name}</h1>
-//       <h2>{propse.age}</h2>
-//     </div>
-//   );
-// };
-// const Inputeforadd = (propse) => {
-//   return (
-//     <>
-//       {" "}
-//       <button>{propse.name}</button>{" "}
-//     </>
-//   );
-// };
 
-const Showclass = (props) => {
-  if (props.finished === true) {
-    return <h2>{props.name}</h2>;
-  }
-};
 export default App;
---
+++ src/componnet.js
@@ -0,0 +1,25 @@
+// // User
+// export const User = (propse) => {
+//   return (
+//     <div>
+//       <h1>{propse.name}</h1>
+//       <h2>{propse.age}</h2>
+//     </div>
+//   );
+// };
+
+// // Inputeforadd
+// export const Inputeforadd = (propse) => {
+//   return (
+//     <>
+//       {" "}
+//       <button>{propse.name}</button>{" "}
+//     </>
+//   );
+// };
+// // Show class
+// export const Showclass = (props) => {
+//   if (props.finished === true) {
+//     return <h2>{props.name}</h2>;
+//   }
+// };
